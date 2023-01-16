806219e23e62eae75486d75aee6a3a0c2c7a5f08
Akash Devgan <akashdevgan95@gmail.com>
2023-01-16 06:55:53 -0500

    removed prefetch false

--- app/types/page.js
+++ app/types/page.js
@@ -15,7 +15,6 @@ const page = async () => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5 server">
       {areas.map((area, idx) => (
         <Link
-          prefetch={false}
           key={idx}
           className="server shadow-gray-50 bg-gray-300 capitalize text-center rounded text-2xl py-10 cursor-pointer font-bold hover:bg-blue-500 hover:text-white"
           href={`/types/${area}`}
